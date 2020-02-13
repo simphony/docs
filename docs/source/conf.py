9c7e2f348ad57f36cd99a272cdadba30c93ddf66
Pablo de Andres <pablo.de.andres@iwm.fraunhofer.de>
2020-02-13 16:36:01 +0100

    Fixed version to alpha

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, MatInformatics team at Fraunhofer IWM'
 author = 'Materials Data Science and Informatics team at Fraunhofer IWM'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.0-alpha'
 
 # -- General configuration ---------------------------------------------------
 
