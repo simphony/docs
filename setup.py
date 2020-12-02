dfcdd9abc94ed8303bbdbf74b0c41f1df7d04e9d
dea <pablo.de.andres@iwm.fraunhofer.de>
2020-12-02 17:44:41 +0100

    removed duplicated requirements

--- setup.py
+++ setup.py
@@ -29,20 +29,6 @@ setup(
     description='The SimPhoNy documentation',
     keywords='simphony, documentation, sphinx, cuds, Fraunhofer IWM',
     long_description=README_TEXT,
-    install_requires=[
-        'osp-core>=' + OSP_CORE_MIN + ', <' + OSP_CORE_MAX,
-        'sphinx',
-        'recommonmark',
-        'sphinxcontrib-plantuml',
-        'nbsphinx',
-        'sphinx-copybutton',
-        'sphinx-jsonschema',
-        'sphinx_rtd_theme',
-        'ipython',
-    ],
-    setup_requires=[
-        'sphinx',
-    ],
     cmdclass={'install': CustomBuild},
     command_options={
         'install': {
